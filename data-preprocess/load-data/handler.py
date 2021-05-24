4f15fee47f45358de283544d1fcaa8ffc422f5d0
root <root@ming-4.maas>
2021-05-24 13:44:18 +0000

    test26

--- data-preprocess/load-data/handler.py
+++ data-preprocess/load-data/handler.py
@@ -1,10 +1,6 @@
 import pandas as pd 
 import json
 def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """
     json_req = json.loads(req)
     file_path = requests.get(json_req["path"])
     data = pd.read_csv(file_path).copy()
